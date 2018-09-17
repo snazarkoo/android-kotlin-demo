bfcd7d177182bef74e0593d9a0174eb84a958826
Nazar Shcherbiak <nazar.shcherbiak@c-map.com>
2018-09-17 21:26:12 +0300

    Init structure

---
+++ app/src/main/java/com/example/nay/androidkotlindemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nay.androidkotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
